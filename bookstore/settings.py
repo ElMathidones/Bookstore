42de376e0c48237d6081f4781e3e0c44972895d8
ElMathidones <seuemail@exemplo.commath.mendez.007@gmail.com>
2026-06-09 22:36:48 -0300

    Add PythonAnywhere deployment support

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "sua-chave-local-de-desenvolvimento")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".herokuapp.com",
+    "elmathidones.pythonanywhere.com",
+]
 
 
 # Application definition
@@ -79,16 +84,24 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bookstore',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
+if os.environ.get("PYTHONANYWHERE") == "true":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "bookstore",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "db",
+            "PORT": "5432",
+        }
+    }
 
 
 # Password validation
